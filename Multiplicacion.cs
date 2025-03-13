ea15920518c8455f1ac4e953f90af8397b165b85
IsabellaP23 <isabella.pena473@pascualbravo.edu.co>
2025-03-13 17:31:10 -0500

    Update Multiplicacion.cs

--- Multiplicacion.cs
+++ Multiplicacion.cs
@@ -5,6 +5,13 @@ namespace NachoMultiplica
 {
     public class Multiplicacion
     {
+        // <summary>
+        // Método Multiplicar
+        // </summar
+        // <param name="a"> Operando1 </param>
+        // <param name="b"> Operando2 </param>
+        // <returns></returns>
+        
         public static long Multiplicar(long a, long b)
         {
             return (a * b);
